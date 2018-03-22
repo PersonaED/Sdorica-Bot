ea5b303dc23d23299d9ee7327c9158aa2c632d83
kjzxc <37648614+kjzxc@users.noreply.github.com>
2018-03-23 02:27:44 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,8 +27,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
-            case 'ping':
+            // !angeliar
+            case 'angeliar':
                 bot.sendMessage({
                     to: channelID,
                     message: 'Angelia R: Thy Fair Lady'
