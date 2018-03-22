a9acb95f558b81d35fbc30a3fcf19e71f8638528
kjzxc <37648614+kjzxc@users.noreply.github.com>
2018-03-23 02:22:15 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'ping':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Pong!'
+                    message: 'Angelia R: Thy Fair Lady'
                 });
             break;
             // Just add any case commands if you want to..
