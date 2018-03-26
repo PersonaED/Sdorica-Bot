5c6a1422c60e304ee6f144a5d26daa19d9d0f289
JayyDex <s4354618@gmail.com>
2018-03-27 02:08:33 +1000

    Added R yan-bo

---
+++ src/assets/characters/yanbo.js
@@ -0,0 +1,28 @@
+export default {
+  "yan-bo r": {
+    "tier": "r",
+    "sprite": "https://i.imgur.com/sT1bN2g.png",
+    "block": "black",
+    "name": "Yan-Bo R: Hidden Figure",
+    "passive": {
+      name: "Free Spirit",
+      "description": "Upon Tier SR, unlock passive skill"
+    },
+    "advisor": {
+      name: "Psyche Relic",
+      "description": "Allies 4 Block skill power increased by 20%"
+    },
+    "1B": {
+      name: "Live On Air",
+      "description": "Grant self Enhance. Switch mode. \n_Enhance: Skill power increased by 30% for each stack. 3 stacks max_"
+    },
+    "2B": {
+      name: "Dead Beat Air",
+      "description": "If ranged offense mode, Attack (x1) all non front row enemies. If single offense mode, Attack (x3) front row enemy, grant self 3 stacks Exhaust for 3 turns. \n_Exhaust: Skill power decreased by 30% for each stack. 3 stacks max_"
+    },
+    "4B": {
+      name: "Airbrush Gush",
+      "description": "Attack (x3) front row enemy"
+    },
+  },
+};
