7e7b99c17602b8cdc388893bbe34916ff2663921
kjzxc <37648614+kjzxc@users.noreply.github.com>
2018-03-28 21:43:42 +0800

    Update nigel.js

--- src/assets/characters/nigel.js
+++ src/assets/characters/nigel.js
@@ -84,7 +84,7 @@ export default {
   },
   'nigel sp-unreleased': {
     tier: TIER_SSR,
-    sprite: 'https://i.imgur.com/YFtmC85.png',
+    sprite: 'https://i.imgur.com/425bBQZ.png',
     block: GOLD,
     name: '?????',
     passive: {
@@ -111,7 +111,7 @@ export default {
   },
   'nigel skin-unreleased': {
     tier: TIER_SSR,
-    sprite: 'https://i.imgur.com/425bBQZ.png',
+    sprite: 'https://i.imgur.com/2D00IVa.png',
     block: GOLD,
     name: '?????',
     passive: {
