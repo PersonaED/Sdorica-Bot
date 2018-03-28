a495ba8c7f2350544e6e27a6e5e12d2b9de17eb1
guguBot <37826297+guguBot@users.noreply.github.com>
2018-03-28 16:52:04 +0800

    Pang with client desc and multipliers

---
+++ src/assets/characters/pang.js
@@ -0,0 +1,126 @@
+import { GOLD } from '../constants.js';
+
+export default {
+ "pang n": {
+    "tier": "n",
+    "sprite": "https://i.imgur.com/IgajiVH.png",
+    "block": GOLD,
+    "name": "Pang N: Amateur Hour",
+    "passive": {
+      "name": "Merciless Meridians",
+      "description": "Upon Tier SR, unlock passive skill"
+    },
+    "advisor": {
+      "name": "Acupoint Attack",
+      "description": "Attack (:crossed_swords: x 1.5) front row enemy (CD: 7)"
+    },
+    "1B": {
+      "name": "One Paw Man",
+      "description": "Attack (:crossed_swords: x 0.75) front row enemy. Grant self Damage Reduction for 2 turns"
+    },
+    "2B": {
+      "name": "Powerful Punch",
+      "description": "Attack (:crossed_swords: x 1.5) front row enemy"
+    },
+    "4B": {
+      "name": "Paws Won't Pause",
+      "description": "Attack (:crossed_swords: x 3) front row enemy"
+    },
+   "status": [
+      "Damage Reduction: Upon incoming damage skill, decrease damage taken by 30% for each stack. 3 stacks max",
+    ],
+  },
+ "pang r": {
+    "tier": "r",
+    "sprite": "https://i.imgur.com/IgajiVH.png",
+    "block": GOLD,
+    "name": "Pang R: Self Made Fan",
+    "passive": {
+      "name": "Merciless Meridians",
+      "description": "Upon Tier SR, unlock passive skill"
+    },
+    "advisor": {
+      "name": "Acupoint Attack",
+      "description": "Attack (:crossed_swords: x 1.5) front row enemy (CD: 7)"
+    },
+    "1B": {
+      "name": "One Paw Man",
+      "description": "Attack (:crossed_swords: x 0.75) front row enemy. Grant self Damage Reduction for 2 turns"
+    },
+    "2B": {
+      "name": "Powerful Punch",
+      "description": "Attack (:crossed_swords: x 1.5) front row enemy. Grant self 1 stack Winded Up"
+    },
+    "4B": {
+      "name": "Paws Won't Pause",
+      "description": "Attack (:crossed_swords: x 3) front row enemy. Grant self 2 stacks Enhance for 3 turns"
+    },
+   "status": [
+      "Damage Reduction: Upon incoming damage skill, decrease damage taken by 30% for each stack. 3 stacks max",
+      "Winded Up: Upon 4 stacks, grant 1 extra attack for next skill",
+      "Enhance: Increase skill power by 30% for each stack. 3 stacks max",
+    ],
+  },
+ "pang sr": {
+    "tier": "sr",
+    "sprite": "https://i.imgur.com/noYW61K.png",
+    "block": GOLD,
+    "name": "Pang SR: Notice Me Sensei",
+    "passive": {
+      "name": "Merciless Meridians",
+      "description": "Upon receive Damage Reduction, grant self 1 stack Winded Up"
+    },
+    "advisor": {
+      "name": "Acupoint Attack",
+      "description": "Attack (:crossed_swords: x 1.5) front row enemy (CD: 6)"
+    },
+    "1B": {
+      "name": "One Paw Man",
+      "description": "Attack (:crossed_swords: x 0.75) front row enemy. Grant self Damage Reduction for 2 turns"
+    },
+    "2B": {
+      "name": "Powerful Punch",
+      "description": "Attack (:crossed_swords: x 1.5) front row enemy. Grant self 1 stack Winded Up"
+    },
+    "4B": {
+      "name": "Paws Won't Pause",
+      "description": "Attack (:crossed_swords: x 3) front row enemy. Grant self 2 stacks Enhance for 3 turns"
+    },
+   "status": [
+      "Damage Reduction: Upon incoming damage skill, decrease damage taken by 30% for each stack. 3 stacks max",
+      "Winded Up: Upon 4 stacks, grant 1 extra attack for next skill",
+      "Enhance: Increase skill power by 30% for each stack. 3 stacks max",
+    ],
+  },
+ "pang ssr": {
+    "tier": "ssr",
+    "sprite": "https://i.imgur.com/0xe5c0K.png",
+    "block": GOLD,
+    "name": "Pang SSR: Shaolin Templar",
+    "passive": {
+      "name": "Merciless Meridians",
+      "description": "Upon receive Damage Reduction, grant self 1 or 2 stacks Winded Up"
+    },
+    "advisor": {
+      "name": "Acupoint Attack",
+      "description": "Attack (:crossed_swords: x 1.5) front row enemy (CD: 5)"
+    },
+    "1B": {
+      "name": "One Paw Man",
+      "description": "Attack (:crossed_swords: x 0.75) front row enemy. Grant self Damage Reduction for 2 turns"
+    },
+    "2B": {
+      "name": "Powerful Punch",
+      "description": "Attack (:crossed_swords: x 1.5) front row enemy. Grant self 1 or 2 stacks Winded Up"
+    },
+    "4B": {
+      "name": "Paws Won't Pause",
+      "description": "Attack (:crossed_swords: x 3) front row enemy. Grant self 2 stacks Enhance for 3 turns"
+    },
+   "status": [
+      "Damage Reduction: Upon incoming damage skill, decrease damage taken by 30% for each stack. 3 stacks max",
+      "Winded Up: Upon 4 stacks, grant 1 extra attack for next skill",
+      "Enhance: Increase skill power by 30% for each stack. 3 stacks max",
+    ],
+  },
+};
