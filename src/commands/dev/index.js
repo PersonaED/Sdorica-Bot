6caa666cd1037605633281217062bbd7ae7926b8
kjzxc <37648614+kjzxc@users.noreply.github.com>
2018-04-11 13:52:19 +0800

    Update index.js

--- src/commands/dev/index.js
+++ src/commands/dev/index.js
@@ -2,7 +2,7 @@ import { standardPrefix } from '../../config';
 
 export const helpCommand = (message, splitContent) => {
   if (splitContent[0] === `${standardPrefix}dev`) {
-    const finalString = '**Credits to -** Karen, chengkor, JayDex, guguBird, Malacia, Ionasal \n\n_With the help of the Wiki, English game client, and Chinois_';
+    const finalString = '**Credits to - Members of Sunset Guild:** \nKaren, chengkor, JayDex, guguBird, Malacia, Ionasal \n\n_With the help of the Wiki, English game client, and Chinois_';
     message.channel.send(finalString);
     return true;
   }
