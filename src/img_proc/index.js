a28243f76f2070d6916e36b1aef60a1f0d82764b
tri-chandra <tri.chandra@outlook.com>
2018-04-18 01:50:34 +1000

    Update Murasa's art

--- src/img_proc/index.js
+++ src/img_proc/index.js
@@ -1,6 +1,17 @@
 import Jimp from 'jimp'
 
 const outputPath = './art_pack/output/';
+const charlist = [
+  'angelia', 'aosta', 'charle', 'crushfang', 'dagger',
+  'dylan', 'experiment', 'fatima', 'fredrica', 'golemwalt',
+  'hestia', 'jahan', 'karnulla', 'kitty', 'koll',
+  'leah', 'lio', 'lisa', 'naya', 'ned',
+  'nigel', 'nolva', 'pang', 'puggi', 'roger',
+  'sharice', 'sherlock', 'sione', 'theodore', 'tica',
+  'yami', 'yan-bo',
+];
+
+const spCharlist = ['dylan_sp', 'angelia_sp', 'sione_sp'];
 
 function readFrame(f, cb) {
   const framePath = `./art_pack/default/frame${f}.png`;
@@ -8,7 +19,7 @@ function readFrame(f, cb) {
     cb(frame);
   });
 }
-function processDefault(imgPath, imgName, tier, offsetX=0, offsetY=0) {
+function processDefault(imgPath, imgName, tier, offsetX = 0, offsetY = 0) {
   const result = new Jimp(417, 527, 0x00000000, () => {});
   readFrame(tier, (frame) => {
     Jimp.read('./art_pack/default/mask.png').then((mask) => {
@@ -19,27 +30,26 @@ function processDefault(imgPath, imgName, tier, offsetX=0, offsetY=0) {
       });
     });
   });
-  // result.write(`${outputPath}output.png`);
-  // readFrame('50', (frame) => {
-  //   Jimp.read(imgPath).then((img) => {
-  //     frame.composite(img, 86, 128).write(`${outputPath}${imgName}.png`);
-  //   });
-  // });
+}
+
+function readMurassaFrame(f, cb) {
+  const framePath = `./art_pack/murasa/${f}.png`;
+  Jimp.read(framePath).then((frame) => {
+    cb(frame);
+  });
+}
+function processMurasa(imgPath, imgName, tier) {
+  const result = new Jimp(223, 321, 0x00000000, () => {});
+  readMurassaFrame(tier, (frame) => {
+    Jimp.read(imgPath).then((img) => {
+      result.blit(img, 0, 0)
+        .composite(frame, 0, 0)
+        .write(`${outputPath}${imgName}_${tier}.png`);
+    });
+  });
 }
 
 function frameDefaultSet() {
-  const charlist = [
-    'angelia', 'aosta', 'charle', 'crushfang', 'dagger',
-    'dylan', 'experiment', 'fatima', 'fredrica', 'golemwalt',
-    'hestia', 'jahan', 'karnulla', 'kitty', 'koll',
-    'leah', 'lio', 'lisa', 'naya', 'ned',
-    'nigel', 'nolva', 'pang', 'puggi', 'roger',
-    'sharice', 'sherlock', 'sione', 'theodore', 'tica',
-    'yami', 'yan-bo',
-  ];
-
-  const spCharlist = ['dylan_sp', 'angelia_sp', 'sione_sp'];
-
   for (const c of charlist) {
     let path = `./art_pack/default/ssr/${c}.png`;
     processDefault(path, c, 'ssr');
@@ -58,6 +68,20 @@ function frameDefaultSet() {
   }
 }
 
+function frameMurasaSet() {
+  for (const c of charlist) {
+    let path = `./art_pack/murasa/${c}_sr.png`;
+    processMurasa(path, c, 'sr');
+
+    path = `./art_pack/murasa/${c}_r.png`;
+    processMurasa(path, c, 'r');
+
+    path = `./art_pack/murasa/${c}_n.png`;
+    processMurasa(path, c, 'n');
+  }
+}
+
 export default {
   frameDefaultSet,
+  frameMurasaSet,
 };
