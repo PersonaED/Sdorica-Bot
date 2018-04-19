d8d442bcd90415f709c88981bf85d5b192c625c5
MirTeiwaz <MirTeiwaz@users.noreply.github.com>
2018-04-19 15:02:09 -0400

    Sione SP is a white block char, not a black one
    Originally said Sione SP is black. Now changed to white

--- src/assets/characters/model/SioneSP.js
+++ src/assets/characters/model/SioneSP.js
@@ -1,10 +1,10 @@
 import BaseCharacter from './core/BaseCharacter';
 import CharStat from './core/CharStat';
-import { BLACK } from '../../constants';
+import { WHITE } from '../../constants';
 
 class SioneSP extends BaseCharacter {
   constructor(param) {
-    super('Sione', new CharStat(2520, 11342, BLACK), param);
+    super('Sione', new CharStat(2520, 11342, WHITE), param);
     this.block = this.stat.block;
   }
 }
