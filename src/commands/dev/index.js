1c9555ca82ebe88e68255be0ef42fb97ecb10c37
tri-chandra <tri-chandra@users.noreply.github.com>
2018-05-01 11:39:28 +1000

    Crediting Pnutz

--- src/commands/dev/index.js
+++ src/commands/dev/index.js
@@ -2,7 +2,7 @@ import { standardPrefix } from '../../config';
 
 export const helpCommand = (message, splitContent) => {
   if (splitContent[0] === `${standardPrefix}dev`) {
-    const finalString = '**Credits to - Members of Sunset Guild:** \nKaren, chengkor, JayDex, guguBird, Malacia, Ionasal, Murasa \n\n_With the help of the Wiki, English game client, and Chinois_';
+    const finalString = '**Credits to - Members of Sunset Guild:** \nKaren, chengkor, JayDex, guguBird, Malacia, Ionasal, Murasa \n\n_With the help of the Wiki, English game client, Chinois, and Pnutz_';
     message.channel.send(finalString);
     return true;
   }
