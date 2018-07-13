8ddce2553efa040b42b53508ad188d2c9e836b92
guguBot <37826297+guguBot@users.noreply.github.com>
2018-07-13 10:50:53 +0800

    Update kittyeyes.js
    added kitty sp

--- src/assets/characters/kittyeyes.js
+++ src/assets/characters/kittyeyes.js
@@ -1,4 +1,4 @@
-import { TIER_R, TIER_SR, TIER_SSR, SKILL_BOOK, TIER_N } from '../constants';
+import { TIER_R, TIER_SR, TIER_SSR, SKILL_BOOK, TIER_N, SP } from '../constants';
 import Kittyeyes from './model/Kittyeyes';
 
 export default {
@@ -100,6 +100,31 @@ export default {
     '4B': {
       name: 'Cat Scratch Fever',
       description: 'True Damage (:crossed_swords: x 1) all enemies, grant Game Tag',
+    },
+  }),
+    'kittyeyes sp': new Kittyeyes({
+    tier: SP,
+    sprite: 'https://imgur.com/2fLHa7N',
+    title: 'Sweet Snack',
+    passive: {
+      name: 'A Maid's Duty',
+      description: 'Upon turn, Attack (:crossed_swords: x 0.5)(:crossed_swords: x 0.4) (:crossed_swords: x 0.3) targets with Game Tag multiple times based on number of stacks, remove Game Tag',
+    },
+    advisor: {
+      name: 'Have Some Tea',
+      description: 'Grant all enemies 2 stacks of :VUL for 2 turns (CD: 7)',
+    },
+    '1B': {
+      name: 'Clumsy Cake Mishap',
+      description: 'Heal (:crossed_swords: x 1.62) selected ally, grant 2 stacks of :TEAR, remove 1 white orb',
+    },
+    '2B': {
+      name: 'I Will Protect Master',
+      description: 'Grant self :SUPPORT for 3 turns. If have Support, extend Support to 3 turns, grant self 2 stacks :ENHANCE',
+    },
+    '4B': {
+      name: 'Blushing Maid',
+      description: 'Heal (:crossed_swords: x 2.16) selected ally, grant :VIGOR',
     },
   }),
   'kittyeyes skin-unreleased': new Kittyeyes({
