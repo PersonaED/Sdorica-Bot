b8213a03c1ea94c50b206da970d7309283dc3e7c
MirTeiwaz <MirTeiwaz@users.noreply.github.com>
2018-07-25 05:36:47 -0400

    Update karen.js

--- src/assets/characters/karen.js
+++ src/assets/characters/karen.js
@@ -20,11 +20,11 @@ export default {
     },
     '2B': {
       name: 'Fire Away!',
-      description: 'Fire the weapon at the top of the rotation.\\n\\n Machine gun: Armor Penetration (:crossed_swords: x 0.525) random enemy 3 times, remove 1 non-black orb.\\n Flamethrower: Attack (:crossed_swords: x 1) all enemies, remove 1 non-black orb.\\n Laser Blaster: True Damage (:crossed_swords: x 1) selected enemy, remove 1 non-black orb.',
+      description: 'Fire the weapon at the top of the rotation.\n\n Machine gun: Armor Penetration (:crossed_swords: x 0.525) random enemy 3 times, remove 1 non-black orb.\n Flamethrower: Attack (:crossed_swords: x 1) all enemies, remove 1 non-black orb.\n Laser Blaster: True Damage (:crossed_swords: x 1) selected enemy, remove 1 non-black orb.',
     },
     '4B': {
       name: 'Total Armageddon',
-      description: 'Fire all weapons simultaneously.\\n\\n Machine gun: Armor Penetration (:crossed_swords: x 0.525) random enemy 3 times, remove 1 non-black orb.\\n Flamethrower: Attack (:crossed_swords: x 1) all enemies, remove 1 non-black orb.\\n Laser Blaster: True Damage (:crossed_swords: x 1) selected enemy, remove 1 non-black orb.',
+      description: 'Fire all weapons simultaneously.\n\n Machine gun: Armor Penetration (:crossed_swords: x 0.525) random enemy 3 times, remove 1 non-black orb.\n Flamethrower: Attack (:crossed_swords: x 1) all enemies, remove 1 non-black orb.\n Laser Blaster: True Damage (:crossed_swords: x 1) selected enemy, remove 1 non-black orb.',
     },
   }),
   'karen sr': new Karen({
@@ -45,11 +45,11 @@ export default {
     },
     '2B': {
       name: 'Fire Away!',
-      description: 'Fire the weapon at the top of the rotation.\\n\\n Machine gun: Armor Penetration (:crossed_swords: x 0.525) random enemy 3 times, remove 1 non-black orb.\\n Flamethrower: Attack (:crossed_swords: x 1) all enemies, remove 1 non-black orb.\\n Laser Blaster: True Damage (:crossed_swords: x 1) selected enemy, remove 1 non-black orb.',
+      description: 'Fire the weapon at the top of the rotation.\n\n Machine gun: Armor Penetration (:crossed_swords: x 0.525) random enemy 3 times, remove 1 non-black orb.\n Flamethrower: Attack (:crossed_swords: x 1) all enemies, remove 1 non-black orb.\n Laser Blaster: True Damage (:crossed_swords: x 1) selected enemy, remove 1 non-black orb.',
     },
     '4B': {
       name: 'Total Armageddon',
-      description: 'Fire all weapons simultaneously.\\n\\n Machine gun: Armor Penetration (:crossed_swords: x 0.525) random enemy 3 times, remove 1 non-black orb.\\n Flamethrower: Attack (:crossed_swords: x 1) all enemies, remove 1 non-black orb.\\n Laser Blaster: True Damage (:crossed_swords: x 1) selected enemy, remove 1 non-black orb.',
+      description: 'Fire all weapons simultaneously.\n\n Machine gun: Armor Penetration (:crossed_swords: x 0.525) random enemy 3 times, remove 1 non-black orb.\n Flamethrower: Attack (:crossed_swords: x 1) all enemies, remove 1 non-black orb.\n Laser Blaster: True Damage (:crossed_swords: x 1) selected enemy, remove 1 non-black orb.',
     },
   }),
   'karen ssr': new Karen({
@@ -70,7 +70,7 @@ export default {
     },
     '2B': {
       name: 'Fire Away!',
-      description: 'Fire the weapon at the top of the rotation.\\n\\n Machine gun: Armor Penetration (:crossed_swords: x 0.525) random enemy 3 times, remove 1 non-black orb.\\n Flamethrower: Attack (:crossed_swords: x 1) all enemies, remove 1 non-black orb.\\n Laser Blaster: True Damage (:crossed_swords: x 1) selected enemy, remove 1 non-black orb.',
+      description: 'Fire the weapon at the top of the rotation.\n\n Machine gun: Armor Penetration (:crossed_swords: x 0.525) random enemy 3 times, remove 1 non-black orb.\n Flamethrower: Attack (:crossed_swords: x 1) all enemies, remove 1 non-black orb.\n Laser Blaster: True Damage (:crossed_swords: x 1) selected enemy, remove 1 non-black orb.',
     },
     '4B': {
       name: 'Total Armageddon',
