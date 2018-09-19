844f9f2990d61f5205cad2c66813a7c9fc0ebdd5
PersonaED <41671378+PersonaED@users.noreply.github.com>
2018-09-19 20:30:41 +0800

    Added LeahSP

--- src/assets/characters/leah.js
+++ src/assets/characters/leah.js
@@ -1,5 +1,6 @@
-import { TIER_R, TIER_SR, TIER_SSR, SKILL_BOOK } from '../constants';
+import { TIER_R, TIER_SR, TIER_SSR, SKILL_BOOK, SP } from '../constants';
 import Leah from './model/Leah';
+import LeahSP from './model/LeahSP';
 
 export default {
   'leah r': new Leah({
@@ -33,7 +34,7 @@ export default {
     title: 'Con Adolescent',
     passive: {
       name: 'Hare Raising',
-      description: 'Upon turn, grant self 1 stack. Upon 4 stacks, increase skill power by 50% for next skill, reset stacks',
+      description: 'Upon the end of player\'s turn, grant self 1 stack. Upon 4 stacks, increase skill ratio by 50% for next skill, reset stacks',
     },
     advisor: {
       name: 'Funny Bunny',
@@ -58,7 +59,7 @@ export default {
     title: 'Original Prankster',
     passive: {
       name: 'Hare Raising',
-      description: 'Upon turn, grant self 1 stack. Upon 3 stacks, increase skill power by 50% for next skill, reset stacks',
+      description: 'Upon the end of player\'s turn, grant self 1 stack. Upon 3 stacks, increase skill ratio by 50% for next skill, reset stacks',
     },
     advisor: {
       name: 'Funny Bunny',
@@ -102,5 +103,30 @@ export default {
       description: '?????',
     },
     status: [],
+  }),
+  'leah sp': new LeahSP({
+    tier: SP,
+    sprite: 'https://i.imgur.com/8AozfZh.png',
+    title: 'Definitely A Bear',
+    passive: {
+      name: 'Cute Is Justice',
+      description: 'Upon player\'s turn, cast skill that has 40% chance to Heal ally with the lowest %HP. Heal amount is equal to 30% of target\'s max HP.',
+    },
+    advisor: {
+      name: 'Fluffy Goodness',
+      description: 'Heal ally with the lowest %HP. Heal amount is equal to 30% of target\'s max HP. (CD: 3)',
+    },
+    '1B': {
+      name: 'Adorable Little Rebel',
+      description: 'Attack (:crossed_swords: x 0.1) gold character and (:crossed_swords: x 1.3) front row enemy.',
+    },
+    '2B': {
+      name: 'Hop Step Jump',
+      description: 'Attack (:crossed_swords: x 0.2) gold and black characters, then Attack (:crossed_swords: x 2.6) front row enemy.',
+    },
+    '4B': {
+      name: 'Fly To The Moon!',
+      description: 'Attack (:crossed_swords: x 0.4) gold and black characters, then Attack (:crossed_swords: x 2.6) front and back row enemies.',
+    },
   }),
 };
