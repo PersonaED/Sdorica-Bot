3c77197cc42a7dc03fe6a031fc2b780c8b9ec0bc
MirTeiwaz <MirTeiwaz@users.noreply.github.com>
2018-10-04 21:34:44 -0300

    Create elio.js

---
+++ src/assets/characters/elio.js
@@ -0,0 +1,80 @@
+import { TIER_R, TIER_SR, TIER_SSR } from '../constants';
+import Crushfang from './model/Elio';
+
+export default {
+  'elio r': new Elio({
+    tier: TIER_R,
+    sprite: 'https://i.imgur.com/MhLEY7g.png',
+    title: 'Ideal Student',
+    passive: {
+      name: 'Magnetization Effect',
+      description: 'Allied Heal effects on Elio have their skill power decreased by 60%.',
+    },
+    advisor: {
+      name: 'Breakthrough Atmosphere',
+      description: 'Upon cast damage skill by ally, if target has Armor, increase skill power by 25%.',
+    },
+    '1B': {
+      name: 'Meteor Shower',
+      description: 'Grant all enemies :VUL and :EXH.',
+    },
+    '2B': {
+      name: 'Cosmic Impact',
+      description: 'True Damage (:crossed_swords: x 1.5) back row enemy. IF target has Armor before this skill cast, grant self (:crossed_swords: x 1.2) Armor as well.',
+    },
+    '4B': {
+      name: 'Infinite Solar Flare',
+      description: 'Attack (:crossed_swords: x 1.2) all enemies, then Attack (:crossed_swords: x 1.2) random enemy 3 times. 2 orbs become gold.',
+    },
+  }),
+  'elio sr': new Elio({
+    tier: TIER_SR,
+    sprite: 'https://i.imgur.com/JendFyT.png',
+    title: 'Outstanding Alumni',
+    passive: {
+      name: 'Magnetization Effect',
+      description: 'Allied Heal effects on Elio have their skill power decreased by 45%.',
+    },
+    advisor: {
+      name: 'Breakthrough Atmosphere',
+      description: 'Upon cast damage skill by ally, if target has Armor, increase skill power by 30%.',
+    },
+    '1B': {
+      name: 'Meteor Shower',
+      description: 'Grant all enemies :VUL and :EXH. 1 orb becomes gold.',
+    },
+    '2B': {
+      name: 'Cosmic Impact',
+      description: 'True Damage (:crossed_swords: x 1.5) back row enemy. IF target has Armor before this skill cast, grant self (:crossed_swords: x 1.2) Armor as well.',
+    },
+    '4B': {
+      name: 'Infinite Solar Flare',
+      description: 'Attack (:crossed_swords: x 1.2) all enemies, then Attack (:crossed_swords: x 1.2) random enemy 3 times. 2 orbs become gold.',
+    },
+  }),
+  'elio ssr': new Elio({
+    tier: TIER_SSR,
+    sprite: 'https://i.imgur.com/QyDkfs9.png',
+    title: 'Exemplary Model',
+    passive: {
+      name: 'Magnetization Effect',
+      description: 'Allied Heal effects on Elio have their skill power decreased by 30%.',
+    },
+    advisor: {
+      name: 'Breakthrough Atmosphere',
+      description: 'Upon cast damage skill by ally, if target has Armor, increase skill power by 35%.',
+    },
+    '1B': {
+      name: 'Meteor Shower',
+      description: 'Grant all enemies :VUL and :EXH. 1 orb becomes gold.',
+    },
+    '2B': {
+      name: 'Cosmic Impact',
+      description: 'True Damage (:crossed_swords: x 1.5) back row enemy. IF target has Armor before this skill cast, grant self (:crossed_swords: x 1.2) Armor and :ARMORSHIFT as well.',
+    },
+    '4B': {
+      name: 'Infinite Solar Flare',
+      description: 'Attack (:crossed_swords: x 1.2) all enemies, then Attack (:crossed_swords: x 1.2) random enemy 3 times. 2 orbs become gold.',
+    },
+  }),
+};
