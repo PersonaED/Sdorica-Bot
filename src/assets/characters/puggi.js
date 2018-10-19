b1defa811abff91d5f995fff4cc4f7c85c95d38a
PersonaED <41671378+PersonaED@users.noreply.github.com>
2018-10-19 22:54:49 +0800

    Updated BPuggi 4o

--- src/assets/characters/puggi.js
+++ src/assets/characters/puggi.js
@@ -124,7 +124,7 @@ export default {
     },
     '4B': {
       name: 'All Hail Puggi!',
-      description: 'Trigger all allies 4-orb skill. Reset all enemies\' CD.',
+      description: 'Grant all allies :EXH for turns, then trigger all allies\' 4-orb skills.',
     },
   }),
 };
