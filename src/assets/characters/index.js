f776a1ecc290847d07027e1642970423e21ec07a
Jason Pham <s4354618@gmail.com>
2018-11-03 11:28:38 +1000

    Add elio to master list

--- src/assets/characters/index.js
+++ src/assets/characters/index.js
@@ -1,38 +1,39 @@
-import angelia from './angelia';
-import aosta from './aosta';
-import charle from './charle';
-import crushfang from './crushfang';
-import dagger from './dagger';
-import dylan from './dylan';
-import experiment from './experiment';
-import fatima from './fatima';
-import fredrica from './fredrica';
-import golemwalt from './golemwalt';
-import hestia from './hestia';
-import izumi from './izumi';
-import jahan from './jahan';
-import karen from './karen';
-import karnulla from './karnulla';
-import kittyeyes from './kittyeyes';
-import koll from './koll';
-import leah from './leah';
-import lio from './lio';
-import lisa from './lisa';
-import naya from './naya';
-import ned from './ned';
-import nigel from './nigel';
-import nolva from './nolva';
-import pang from './pang';
-import puggi from './puggi';
-import roger from './roger';
-import sherlock from './sherlock';
-import shirley from './shirley';
-import sharice from './sharice';
-import sione from './sione';
-import theodore from './theodore';
-import tica from './tica';
-import yamitsuki from './yamitsuki';
-import yanbo from './yanbo';
+import angelia from "./angelia";
+import aosta from "./aosta";
+import charle from "./charle";
+import crushfang from "./crushfang";
+import dagger from "./dagger";
+import dylan from "./dylan";
+import elio from "./elio";
+import experiment from "./experiment";
+import fatima from "./fatima";
+import fredrica from "./fredrica";
+import golemwalt from "./golemwalt";
+import hestia from "./hestia";
+import izumi from "./izumi";
+import jahan from "./jahan";
+import karen from "./karen";
+import karnulla from "./karnulla";
+import kittyeyes from "./kittyeyes";
+import koll from "./koll";
+import leah from "./leah";
+import lio from "./lio";
+import lisa from "./lisa";
+import naya from "./naya";
+import ned from "./ned";
+import nigel from "./nigel";
+import nolva from "./nolva";
+import pang from "./pang";
+import puggi from "./puggi";
+import roger from "./roger";
+import sherlock from "./sherlock";
+import shirley from "./shirley";
+import sharice from "./sharice";
+import sione from "./sione";
+import theodore from "./theodore";
+import tica from "./tica";
+import yamitsuki from "./yamitsuki";
+import yanbo from "./yanbo";
 
 export {
   angelia,
@@ -41,6 +42,7 @@ export {
   crushfang,
   dagger,
   dylan,
+  elio,
   experiment,
   fatima,
   fredrica,
@@ -69,5 +71,5 @@ export {
   theodore,
   tica,
   yamitsuki,
-  yanbo,
+  yanbo
 };
