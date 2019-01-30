2e8d4ccce76256ad74c130bead7ae6f8960b4ebd
PersonaED <41671378+PersonaED@users.noreply.github.com>
2019-01-30 14:58:30 +0800

    Fixed typo "pang" -> "hestia"

--- src/assets/characters/model/HestiaSP.js
+++ src/assets/characters/model/HestiaSP.js
@@ -1,10 +1,10 @@
-import BaseCharacter from './core/BaseCharacter';
-import CharStat from './core/CharStat';
-import { BLACK } from '../../constants';
+import BaseCharacter from "./core/BaseCharacter";
+import CharStat from "./core/CharStat";
+import { BLACK } from "../../constant";
 
 class HestiaSP extends BaseCharacter {
   constructor(param) {
-    super('Pang', new CharStat(2520, 10081, BLACK), param);
+    super("Hestia", new CharStat(2520, 10081, BLACK), param);
     this.block = this.stat.block;
   }
 }
