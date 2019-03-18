5256a4f6440f4f9f17fa045f9a980891180fa4b5
Jason Pham <JayyDex@users.noreply.github.com>
2019-03-18 15:12:08 +1000

    Update index.js

--- src/commands/characters/index.js
+++ src/commands/characters/index.js
@@ -30,7 +30,7 @@ export const sendCharacterInfo = (characterInfo, message, infuse) => {
         url: characterInfo.sprite || '-',
       },
       author: {
-        name: characterInfo.name || '-',
+        name: characterInfo.nameOVERRIDE || '-',
       },
       fields: [
         {
